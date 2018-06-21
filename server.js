bfd2b793400c064e2a25d2dcb3467fa3291498c6
Jes Ababon <jesababon@gmail.com>
2018-06-20 23:21:56 -0400

    app.use(express.static(__dirname + '/app'))

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const methodOverride = require("method-override");
 const PORT = process.env.PORT || 4567;
 const app = express();
 
+app.use(express.static(__dirname + '/'));
 
 app.use(methodOverride('_method'));
 
